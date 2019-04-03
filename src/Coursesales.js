eae202f2f5faffd455cf1eafb0e896200ef4e7c9
richa <richardmel@outlook.com>
2019-04-03 11:29:21 -0500

    footer clean-up

--- src/Coursesales.js
+++ src/Coursesales.js
@@ -34,7 +34,7 @@ class Coursesales extends Component {
           {courses}
 
           <p id="total"> Total: <strong>{this.state.total}</strong></p>
-          
+
         </div>
 
         <Course />
--- src/Footer.js
+++ src/Footer.js
@@ -9,32 +9,7 @@ class Footer extends Component {
      <div className="container">
        <div className="row">
          <div className="col-lg-8 col-md-10 mx-auto">
-           <ul className="list-inline text-center">
-             <li className="list-inline-item">
-               <a href="#">
-                 <span className="fa-stack fa-lg">
-                   <i className="fas fa-circle fa-stack-2x"></i>
-                   <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                 </span>
-               </a>
-             </li>
-             <li className="list-inline-item">
-               <a href="#">
-                 <span className="fa-stack fa-lg">
-                   <i className="fas fa-circle fa-stack-2x"></i>
-                   <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                 </span>
-               </a>
-             </li>
-             <li className="list-inline-item">
-               <a href="#">
-                 <span className="fa-stack fa-lg">
-                   <i className="fas fa-circle fa-stack-2x"></i>
-                   <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                 </span>
-               </a>
-             </li>
-           </ul>
+           <div style={{marginTop:'80px'}}></div>
            <p className="copyright text-muted">Copyright &copy; Richard KM 2018</p>
          </div>
        </div>
